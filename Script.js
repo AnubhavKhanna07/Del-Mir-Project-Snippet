dc18234283eb6bf19e8e9ca663240e8c6f1d37d9
AnubhavKhanna98 <53474981+AnubhavKhanna98@users.noreply.github.com>
2020-04-19 21:58:40 +1200

    Finished getUniqueID() function
    Decided to use regex for ID extraction

--- Script.js
+++ Script.js
@@ -1,39 +1,13 @@
-/* Old code
-// 15/04/2020
-
-//Author: Mirlington
-//Wipes facebook's distracting content off the screen.
-function wipeContent() {
-    const fbHeight = window.screen.height - 185;
-    
-    const EMBED_URL = '<p style="text-align: center;"><iframe width="480" height="360" src="https://www.youtube.com/embed/_mkiGMtbrPM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>';
-    
-    const content = document.getElementById('content');
-    content.style.height = fbHeight.toString()+"px";
-    content.style.width = "inherit";
-    content.innerHTML = EMBED_URL;
-    content.appendChild(iframe);
-    return null;
-}
-
-wipeContent()
-
-// 480p = 480x720
-// 360p = 480x360
-*/
-
 //Global constants
-const prefix1 = "https://www.youtube.com/"; //standard
+const prefix1 = "https://www.youtube.com/watch?"; // standard
 const prefix2 = "https://youtu.be/"; // after clicking share
-//const prefix3 = "https://www.youtube.com/watch?time_continue=1"; // after clicking YOUTUBE button on an embedded video (e.g. on Discord)
+const videoIdLength = 11;
 
-// https://www.youtube.com/watch?v=QxHkLdQy5f0
-// https://youtu.be/QxHkLdQy5f0
-// https://www.youtube.com/watch?time_continue=148&v=_mkiGMtbrPM&feature=emb_logo
+// https://www.youtube.com/watch?v=QxHkLdQy5f0 // standard
+// https://youtu.be/QxHkLdQy5f0 // after clicking share
+// https://www.youtube.com/watch?time_continue=148&v=_mkiGMtbrPM&feature=emb_logo // after clicking the YOUTUBE button on a Youtube embed
 
-// to deal with prefix 3 we should perhaps split by "&v="
-
-const videoIDLength = 11;
+// to deal with prefix 3 we should perhaps split by "&"
 
 // Turns the extension off and on
 function toggleState() {
@@ -45,8 +19,22 @@ function toggleState() {
   }
 }
 
-function print() {
-  console.log("Hello, World");
+function printT() {
+  //const correct = "_mkiGMtbrPM";
+  const dummy =
+    "https://www.youtube.com/watch?time_continue=148&v=_mkiGMtbrPM&feature=emb_logo";
+
+  // Regex method
+
+  // Find the first set of 11 characters that abide by the following rules:
+  // 1. There is an '=' before it.
+  // 2. There is an '&' after it.
+  const actuallyGoodRx = /(?<==).{11}(?=&)/;
+  console.log(dummy.match(actuallyGoodRx)[0]); // Index used as match() returns an array
+
+  // split method
+  // const split = dummy.split("&v=");
+  // console.log(split[1].slice(0, videoIdLength));
 }
 
 // Main: actually re-parses the Youtube embed HTML
@@ -59,7 +47,7 @@ function replaceVideo() {
   if (url) {
     // go to checkLink
     console.log(checkLink(url));
-    // go to getUniqueId
+    getUniqueId(url);
     // go to reparseEmbed (doesn't exist yet)
   }
 }
@@ -79,8 +67,45 @@ function checkLink(url) {
 
 // Helper gets the last unique string of a Youtube URL
 function getUniqueId(url) {
-  // Extract for type 1: https://www.youtube.com/watch?v=Gu77Vtja30c
-  // Extract for type 2: https://youtu.be/Gu77Vtja30c
-  // Extractfor type 3: https://www.youtube.com/watch?v=FMrqlo_L-gY&feature=emb_rel_pause
+  const version = "v=";
+  const standard = prefix1 + version; // standard youtube link prefix
+
+  // Extract Id from prefix2 (Easy)
+  if (url.slice(0, prefix2.length) === prefix2) {
+    console.log(url.slice(prefix2.length, prefix2.length + videoIdLength));
+
+    // Extract from prefix1 (Harder): https://www.youtube.com/watch?v=Gu77Vtja30c
+  } else if (url.slice(0, prefix1.length) === prefix1) {
+    // Standard version
+    if (url.slice(0, standard.length) === standard) {
+      console.log(url.slice(standard.length, standard.length + videoIdLength));
+
+      // Embed version (use regex?)
+    } else {
+      // Find the first set of 11 characters that abide by the following rules:
+      // 1. There is an '=' before it.
+      // 2. There is an '&' after it.
+      const regex = /(?<==).{11}(?=&)/;
+      console.log(url.match(regex)[0]); // Index used as match() returns an array
+    }
+  }
+
+  // Extract for type 3: https://www.youtube.com/watch?v=FMrqlo_L-gY&feature=emb_rel_pause
 }
 
+/*
+EMBED CODE
+
+<iframe width="480" height="360" 
+src="https://www.youtube.com/embed/_mkiGMtbrPM" 
+frameborder="0" allow="accelerometer; 
+autoplay;
+encrypted-media;
+gyroscope;
+picture-in-picture" 
+allowfullscreen>
+</iframe>
+*/
+
+/* REGEX FOR TOMORROW (?<=https:\/\/youtu\.be\/).{11} 
+(?<=v=).{11}*/
