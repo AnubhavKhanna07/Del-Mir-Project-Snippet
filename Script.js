f9998fcce5e3f19628d0d8edba17cbe2234de24a
AnubhavKhanna98 <anubhavkhanna.nz@gmail.com>
2020-04-25 21:54:26 +1200

    Made the extension button functional

--- Script.js
+++ Script.js
@@ -1,3 +1,23 @@
+// Code required to make the extension functional (in Google Chrome for a start)
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.facebook.com'},
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+
+// --------------------------------------------------------------
+
+
 //Global constants
 const prefix1 = "https://www.youtube.com/watch?"; // standard
 const prefix2 = "https://youtu.be/"; // after clicking share
